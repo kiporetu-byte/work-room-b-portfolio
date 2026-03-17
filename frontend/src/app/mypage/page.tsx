fd52281d6be685a11d5c00109afeb3947657851b
Kanna-25 <Kanna-25@users.noreply.github.com>
2026-03-17 21:03:42 +0900

    add: CRUD機能追加しました

--- frontend/src/app/mypage/page.tsx
+++ frontend/src/app/mypage/page.tsx
@@ -3,28 +3,42 @@
 import Link from "next/link";
 import Button from "@/components/common/Button";
 import { useEffect, useState } from "react";
-import { getPosts } from "@/lib/api";
-import { Post } from "@/types/article";
+import { getPosts, savePosts, Post } from "@/lib/mockPosts";
+// import { getPosts } from "@/lib/api";　//api繋げるまで一旦コメントアウト
+// import { Post } from "@/types/article"; //api繋げるまで一旦コメントアウト
 
 export default function MyPage() {
-  // 一旦ダミーデータで表示のためコメントアウト
-  // const [posts, setPosts] = useState<Post[]>([]);
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      url: "https://example.com",
-      description: "Reactの勉強にめっちゃ良かった記事",
-      username: "tanaka",
-      created_at: "2024-01-01",
-    },
-    {
-      id: 2,
-      url: "https://nextjs.org",
-      description: "Next.js公式ドキュメント",
-      username: "sato",
-      created_at: "2024-01-02",
-    },
-  ]);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  // ダミーデータ
+  // const [posts, setPosts] = useState([
+  //   {
+  //     id: 1,
+  //     url: "https://example.com",
+  //     memo: "Reactの勉強にめっちゃ良かった記事",
+  //     username: "tanaka",
+  //     created_at: "2024-01-01",
+  //   },
+  //   {
+  //     id: 2,
+  //     url: "https://nextjs.org",
+  //     memo: "Next.js公式ドキュメント",
+  //     username: "sato",
+  //     created_at: "2024-01-02",
+  //   },
+  // ]);
+
+  useEffect(() => {
+    const data = getPosts();
+    setPosts(data);
+  }, []);
+
+  const handleDelete = (id: number) => {
+    const newPosts = posts.filter((p) => p.id !== id);
+    savePosts(newPosts);
+    setPosts(newPosts);
+  };
+
   // 一旦ダミーデータで表示のためコメントアウト
   // useEffect(() => {
   //   const token = localStorage.getItem("token");
@@ -56,15 +70,19 @@ export default function MyPage() {
             className="border rounded p-4 shadow-sm space-y-2"
           >
             <p className="font-bold">{post.url}</p>
-            <p>{post.description}</p>
-            <p className="text-sm text-gray-500">{post.username}</p>
+            <p>{post.memo}</p>
+            <p className="text-sm text-gray-500">user:{post.user_id}</p>
+            <p className="text-sm text-gray-400">{post.created_at}</p>
 
             <div className="flex gap-3">
-              <Link href={`/posts/${post.id}/edit`}>
+              <Link href={`/articles/${post.id}/edit`}>
                 <Button>編集</Button>
               </Link>
 
-              <button className="px-4 py-2 rounded bg-red-600 text-white hover:opacity-90">
+              <button
+                onClick={() => handleDelete(post.id)}
+                className="px-4 py-2 rounded bg-red-600 text-white hover:opacity-90"
+              >
                 削除
               </button>
             </div>
